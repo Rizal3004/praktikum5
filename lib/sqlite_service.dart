ec21eab88af89e8ca6f548dd936459cf78463c25
Rizal3004 <134471146+Rizal3004@users.noreply.github.com>
2023-11-09 22:44:52 +0700

    Update sqlite_service.dart

--- lib/sqlite_service.dart
+++ lib/sqlite_service.dart
@@ -1,58 +1,167 @@
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
-import 'models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:sqlite/models/saham.dart';
+import 'package:sqlite/sqlite_service.dart';
 
-class DatabaseHandler {
-  Future<Database> initializeDB() async {
-    String path = await getDatabasesPath();
-    return openDatabase(
-      join(path, 'example.db'),
-      onCreate: (database, version) async {
-        await database.execute(
-          "CREATE TABLE users("
-              "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-              "name TEXT NOT NULL,age INTEGER NOT NULL, "
-              "country TEXT NOT NULL, "
-              "email TEXT )",
-        );
-      },
-      version: 1,
+class TambahSaham extends StatefulWidget {
+  const TambahSaham({super.key, required this.onPressed});
+
+  final void Function() onPressed;
+
+  @override
+  State<TambahSaham> createState() => _TambahSahamState();
+}
+
+class _TambahSahamState extends State<TambahSaham> {
+  late DatabaseHandler handler;
+
+  final _formKey = GlobalKey<FormState>();
+
+  // Create a text controller. Later, use it to retrieve the
+  // current value of the TextField.
+  final tickerController = TextEditingController();
+  final openController = TextEditingController();
+  final highController = TextEditingController();
+  final lastController = TextEditingController();
+  final changeController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    tickerController.dispose();
+    openController.dispose();
+    highController.dispose();
+    lastController.dispose();
+    changeController.dispose();
+    super.dispose();
+  }
+
+  void tambahSaham() {
+    handler = DatabaseHandler();
+    handler.initializeDB().whenComplete(() async {
+      await addSaham();
+      setState(() {});
+    });
+  }
+
+  Future<int> addSaham() async {
+    List<Saham> listSaham = [
+      Saham(
+          ticker: tickerController.text,
+          open: int.parse(openController.text),
+          high: int.parse(highController.text),
+          last: int.parse(lastController.text),
+          change: changeController.text),
+    ];
+
+    return await handler.insertSaham(listSaham);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tambah Saham'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              Text("Ticker"),
+              TextFormField(
+                controller: tickerController,
+                decoration: const InputDecoration(
+                  hintText: 'Ticker',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Text("Open"),
+              TextFormField(
+                controller: openController,
+                decoration: const InputDecoration(
+                  hintText: 'Open',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  } else if (int.tryParse(value) == null) {
+                    return 'Please enter a number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Text("High"),
+              TextFormField(
+                controller: highController,
+                decoration: const InputDecoration(
+                  hintText: 'High',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  } else if (int.tryParse(value) == null) {
+                    return 'Please enter a number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Text("Last"),
+              TextFormField(
+                controller: lastController,
+                decoration: const InputDecoration(
+                  hintText: 'Last',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  } else if (int.tryParse(value) == null) {
+                    return 'Please enter a number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Text("Change"),
+              TextFormField(
+                controller: changeController,
+                decoration: const InputDecoration(
+                  hintText: 'Change',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  } else
+                    return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    tambahSaham();
+                    widget.onPressed();
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Data saham berhasil disimpan')),
+                    );
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-  }
-
-  Future<int> insertUser(List<User> users) async {
-    int result = 0;
-    final Database db = await initializeDB();
-    for(var user in users){
-      result = await db.insert('users', user.toMap());
-    }
-    return result;
-  }
-
-  Future<void> deleteUser(int? id) async {
-    final Database db = await initializeDB();
-    await db.delete(
-      'users',
-      where: "id = ?",
-      whereArgs: [id],
-    );
-  }
-
-  Future<int> updateUser(User user) async {
-    int result = 0;
-    final Database db = await initializeDB();
-    result = await db.update(
-      'users',
-      user.toMap(),
-      where: "id = ?",
-      whereArgs: [user.id],
-    );
-    return result;
-  }
-
-  Future<List<User>> retrieveUsers() async {
-    final Database db = await initializeDB();
-    final List<Map<String, Object?>> queryResult = await db.query('users');
-    return queryResult.map((e) => User.fromMap(e)).toList();
   }
 }
